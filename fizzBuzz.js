03c61e118f28caa3c070b97fd66c747a5c2283b9
Caleb Beazel <caleb.beazel@gmail.com>
2023-04-12 16:51:18 -0600

    fizzBuzz-1

---
+++ fizzBuzz.js
@@ -0,0 +1,13 @@
+let num = 1
+
+for(i=num; i <= 50; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("fizzbuzz")
+    } else if(i % 3 === 0){
+        console.log("fizz")
+    } else if(i % 5 === 0){
+        console.log("buzz")
+    } else{
+        console.log(i)
+    }
+}
